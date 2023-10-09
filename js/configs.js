f5b503f80fdd4ce28f44c8845bce8412d50d48b6
fernandopinhel <fernandopinhelll@gmail.com>
2023-10-09 15:19:58 -0300

    Subindo diretiva de privacidade

--- js/configs.js
+++ js/configs.js
@@ -27,3 +27,19 @@ $(document).ready(function(){
     duration: 800,
     once: true
 });
+
+// Cookies
+const $sectionCookies = document.querySelector('.cookies');
+const $btnCookies = document.querySelector('.btn-closed-cookies');
+
+// Condicional pengando os 7 dias que o usuário acessou e aceitou o cookies e não mostrando novamente a msg para confirmar.
+if (Cookies.get('closed')) {
+    $sectionCookies.classList.remove('is-open')
+}
+
+$btnCookies.addEventListener('click', function() {
+    // 7 dias para expirar a sessão do usuário e voltar a aparecer a confirmação de cookies.
+    Cookies.set('closed', true, { expires: 7 })
+    $sectionCookies.classList.remove('is-open')
+})
+
